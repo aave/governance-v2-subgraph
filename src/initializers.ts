b079cdb6af9e47fa37ec2c98e84eb1e22e0d0342
dhadrien <hadrien@variabl.io>
2020-12-10 14:09:17 +0100

    Created Minimal subgraph, shcheme, mapping for user centric vote fetching

---
+++ src/initializers.ts
@@ -0,0 +1,39 @@
+import { Bytes } from '@graphprotocol/graph-ts';
+import {
+  Proposal,
+} from './generated/schema';
+import {
+  zeroAddress,
+  zeroBI,
+} from './utils/converters';
+import { NA, PROPOSAL_STATUS_INITIALIZING } from './utils/constants';
+
+
+export function getOrInitProposal(proposalId: string): Proposal {
+  let proposal = Proposal.load(proposalId);
+
+  if (!proposal) {
+    proposal = new Proposal(proposalId);
+    proposal.title = NA;
+    proposal.shortDescription = NA;
+    proposal.creator = Bytes.fromI32(0) as Bytes;
+    proposal.executor = Bytes.fromI32(0) as Bytes;
+    proposal.targets = [Bytes.fromI32(0) as Bytes];
+    proposal.values = [zeroBI()];
+    proposal.signatures = [NA];
+    proposal.calldatas = [Bytes.fromI32(0) as Bytes];
+    proposal.withDelegatecalls = [false];
+    proposal.startBlock = zeroBI();
+    proposal.endBlock = zeroBI();
+    proposal.governanceStrategy = Bytes.fromI32(0) as Bytes;
+    proposal.currentNoVote = zeroBI();
+    proposal.currentYesVote = zeroBI();
+    proposal.winner = NA;
+    proposal.createdTimestamp = zeroBI().toI32();
+    proposal.lastUpdateBlock = zeroBI();
+    proposal.lastUpdateTimestamp = zeroBI().toI32();
+    proposal.ipfsHash = NA;
+  }
+
+  return proposal as Proposal;
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const PROPOSAL_STATUS_INITIALIZING = 'Initializing';
+export const PROPOSAL_STATUS_VOTING = 'Voting';
+export const PROPOSAL_STATUS_VALIDATING = 'Validating';
+export const PROPOSAL_STATUS_EXECUTED = 'Executed';
+export const YES_WINS = 'Yes';
+export const NO_WINS = 'No';
+export const ABSTAIN_WINS = 'Abstain';
+export const NA = 'Na';
