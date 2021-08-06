7dc69341bbcacd86a4a5bf07efcc0cceeaacfa2c
Andrew Schmidt <andrew@aave.com>
2021-08-05 23:55:29 -0500

    feat: add constants for enum

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -23,3 +23,8 @@ export const YES_WINS = 'Yes';
 export const NO_WINS = 'No';
 export const ABSTAIN_WINS = 'Abstain';
 export const NA = 'Na';
+
+export const VOTING_CONSTANT = 'Voting';
+export const PROPOSITION_CONSTANT = 'Proposition';
+export const AAVE_CONSTANT = 'Aave';
+export const STKAAVE_CONSTANT = 'StkAave';
